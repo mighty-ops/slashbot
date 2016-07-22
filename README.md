d30ee56710a231c806ac680e81c0581d42841f45
Andrew Aitken-Fincham <andrew@silverstripe.co.nz>
2016-07-22 17:51:42 +1200

    Am I feeling lucky, punk?

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# slashbot-example
-An example slashbot. Fork me!
+## The SS-Slashbot
+
+This project is a fork!
 
 For full docs on how to use this repo, head over to the [slashbot repo](https://github.com/jesseditson/slashbot)
+
+
+Currently hosted at ss-slashbot.herokuapp.com.
+
+All deploys to master are automatically deployed to Heroku.
--- responders/lmgtfy.js
+++ responders/lmgtfy.js
@@ -1,5 +1,6 @@
 var lmgtfy = function(message,callback) {
-    var link = 'https://www.google.com/?#q=' + encodeURIComponent(message)
+    var query = encodeURIComponent(message)
+    var link = 'https://www.google.com/search?site=&source=hp&q=' + query + "&oq=" + query + "&gs_1"
     callback(null,'LMGTFY: ' + link)
 }
 
@@ -7,3 +8,6 @@ var lmgtfy = function(message,callback) {
 lmgtfy.match = /^how do I /i
 
 module.exports = lmgtfy
+
+
+search?site=&source=hp&q=things&oq=things&gs_l
